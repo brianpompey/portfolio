94a5964b12459eeb3352fbd0e53dc7f3cf5bdb9c
brianpompey <brianthescholar@gmail.com>
2021-03-12 14:49:36 -0500

    working on card components

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Card = () => (
+    <div className="Card">
+        <h3>Twitter Clone</h3>
+        <p>Rails project with a ReactJS Backend</p>
+    </div>
+)
+
+export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Card from "../components/Card"
 
 const IndexPage = () => (
   <Layout>
@@ -12,12 +13,21 @@ const IndexPage = () => (
         <p>Welcome to my portfolio page.</p>
         <p>This is where I get to showcase all the</p>
         <p>Wonderful work that I have been doing!</p>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#f3f4f5" fill-opacity="1" >
+            <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="M0,32L40,64C80,96,160,160,240,186.7C320,213,400,203,480,181.3C560,160,640,128,720,112C800,96,880,96,960,106.7C1040,117,1120,139,1200,122.7C1280,107,1360,53,1400,26.7L1440,0L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z" />
+          </path>
+        </svg>
+      </div>
+      <div className="Cards">
+        <h2>Now Let's Look at Some Projects!</h2>
+        <Card 
+          title="Twitter Clone"
+          text="Rails project with a ReactJS Backend"
+          image={require('../images/twittercon.jpg')}/>
       </div>
     </div>
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
   </Layout>
 )
 
