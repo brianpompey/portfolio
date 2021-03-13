f51b1b4bde98c7aed69aa993cd07d7c8d9da2a97
brianpompey <brianthescholar@gmail.com>
2021-03-13 11:35:33 -0500

    working on header font issues

--- src/components/header.js
+++ src/components/header.js
@@ -4,18 +4,45 @@ import { Link } from "gatsby"
 import './Header.css'
 import logo from '../images/brian-logo.png'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={logo} width="30" alt="brian-logo" /></Link>
-      <Link to="/about">About Me</Link>
-      <Link to="/resume">My Resume</Link>
-      <Link to="/blogs">Blog Posts</Link>
-      <Link to="/github">Github Page</Link>
-      <Link to="/contact"><button>Hire ME</button></Link>
-    </div>
-  </div>
-)
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return(
+      <div className="Header">
+        <div className="HeaderGroup">
+          <Link to="/"><img src={logo} width="30" alt="brian-logo" /></Link>
+          <Link to="/about">About Me</Link>
+          <Link to="/resume">My Resume</Link>
+          <Link to="/blogs">Blog Posts</Link>
+          <Link to="/github">Github Page</Link>
+          <Link to="/contact"><button>Hire ME</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
