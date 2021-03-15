fa63d9f2856c48a2cac09df180dc6fe88b077c96
brianpompey <brianthescholar@gmail.com>
2021-03-15 19:11:54 -0400

    gonna import the layout over instead of individual components works so much better

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import './Contact.css'
-import Header from "./header"
+import './layout'
+import Layout from './layout';
+
 
 const Contact = () => (
-    <div className="ContactGroup">
-        <Header/>
-        <div className="ContactInfo">
-        <h2>If you want to get in touch with or hire me <br /> (You Should!)...</h2>
-            <p>Email: brianthescholar@gmail.com</p>
-            <p>Alt Email: bpompey123@gmail.com</p>
-            <p>Phone: (718) 578-8791</p>
-            <p> LinkedIN Page: <a href="https://www.linkedin.com/in/brian-pompey-826a58165/" target="_blank">Brian Pompey LinkedIN</a></p>
+    <Layout>
+        <div className="ContactGroup">
+            
+            <div className="ContactInfo">
+            <h2>If you want to get in touch with or hire me <br /> (You Should!)...</h2>
+                <p>Email: brianthescholar@gmail.com</p>
+                <p>Alt Email: bpompey123@gmail.com</p>
+                <p>Phone: (718) 578-8791</p>
+                <p> LinkedIN Page: <a href="https://www.linkedin.com/in/brian-pompey-826a58165/" target="_blank">Brian Pompey LinkedIN</a></p>
+            </div>
+            
         </div>
-    </div>
+    </Layout>
 )
 
 export default Contact;
