7d9855709f2b5eea930f721fc580025f1e28536a
brianpompey <brianthescholar@gmail.com>
2021-03-15 21:44:07 -0400

    working on about page

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './About.css'
+import './layout'
+import Layout from './layout';
+
+
+const About = () => (
+    <Layout>
+        <div className="AboutGroup">
+            
+            <div className="AboutText">
+            <h2>If you want to get in touch with or hire me <br /> (You Should!)...</h2>
+                <p>Email: brianthescholar@gmail.com</p>
+                <p>Alt Email: bpompey123@gmail.com</p>
+                <p>Phone: (718) 578-8791</p>
+                <p> LinkedIN Page: <a href="https://www.linkedin.com/in/brian-pompey-826a58165/" target="_blank">Brian Pompey LinkedIN</a></p>
+            </div>
+            
+        </div>
+    </Layout>
+)
+
+export default About;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,15 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import About from "../components/About"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const About = () => (
-    <Layout>
+const AboutPage = () => (
+    <div>
         <SEO title="About" />
+        <About/>
         <Link to="/">Go back to the homepage</Link>
-    </Layout>
+    </div>
 )
 
-export default About;
+export default AboutPage;
