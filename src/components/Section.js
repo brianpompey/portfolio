efc30feb2f09854a02060df656ab256bd0903661
brianpompey <brianthescholar@gmail.com>
2021-03-16 16:59:55 -0400

    working on svg wave

--- src/components/Section.js
+++ src/components/Section.js
@@ -26,3 +26,31 @@ const Section = props => (
 )
 
 export default Section
+
+const SectionCaption = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #94A4BA;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  padding: 0 20px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
+
+<SectionCaption>12 sections - 6 hours</SectionCaption>
+<SectionCellGroup>
+  {staticdata.cells.map(cell => (
+    <Cell title={cell.title} image={cell.image} />
+  ))}
+</SectionCellGroup>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,35 +4,12 @@ import twittercon from "../images/twittercon.jpg"
 import bron from "../images/Lebron_James.jpg"
 import kobe from "../images/Kobe.jpg"
 import mj from "../images/michael-jordan.jpg"
-import staticdata from '../../staticdata.json'
 
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Card from "../components/Card"
-import Cell from "../components/Cell"
-import styled from 'styled-components'
 
-const SectionCaption = styled.p`
-  font-weight: 600;
-  font-size: 18px;
-  text-transform: uppercase;
-  color: #94A4BA;
-  text-align: center;
-`
-
-const SectionCellGroup = styled.div`
-  max-width: 800px;
-  margin: 0 auto 100px;
-  padding: 0 20px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-column-gap: 20px;
-
-  @media (max-width: 800px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
-`
 
 const IndexPage = () => (
   <Layout>
@@ -43,9 +20,17 @@ const IndexPage = () => (
         <p>This is where I get to showcase all the</p>
         <p>Wonderful work that I have been doing!</p>
 
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="172" viewBox="0 0 100% 172">
           <path fill="#f3f4f5" fill-opacity="1" >
-            <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="M0,32L40,64C80,96,160,160,240,186.7C320,213,400,203,480,181.3C560,160,640,128,720,112C800,96,880,96,960,106.7C1040,117,1120,139,1200,122.7C1280,107,1360,53,1400,26.7L1440,0L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z" />
+          <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="M0,128L48,117.3C96,107,192,85,288,112C384,139,480,213,576,208C672,203,768,117,864,80C960,43,1056,53,1152,58.7C1248,64,1344,64,1392,64L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z;
+          
+          M0,256L48,224C96,192,192,128,288,133.3C384,139,480,213,576,240C672,267,768,245,864,250.7C960,256,1056,288,1152,266.7C1248,245,1344,171,1392,133.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z;
+          
+          M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,117.3C672,139,768,213,864,218.7C960,224,1056,160,1152,138.7C1248,117,1344,139,1392,149.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z;
+          
+          M0,128L48,117.3C96,107,192,85,288,112C384,139,480,213,576,208C672,203,768,117,864,80C960,43,1056,53,1152,58.7C1248,64,1344,64,1392,64L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z
+          "
+          />
           </path>
         </svg>
       </div>
@@ -53,7 +38,8 @@ const IndexPage = () => (
     <div className="IndexRestofPage">
       <div className="Cards">
           <h2>Now Let's Look at Some Projects!</h2>
-          <div className="CardGroup">
+          <h3>Bootcamp Projects</h3>
+          <div className="CardGroup-1">
             <Card 
               title="Twitter Clone"
               text="Rails project with a ReactJS Backend"
@@ -71,14 +57,28 @@ const IndexPage = () => (
               text="MJ"
               image={ mj } />
           </div>
+          <hr class="rounded" />
+          <h3>Personal Projects</h3>
+          <div className="CardGroup-2">
+            <Card 
+              title="Twitter Clone"
+              text="Rails project with a ReactJS Backend"
+              image={ twittercon } />
+              <Card 
+              title="Lebron James"
+              text="King James"
+              image={ bron } />
+            <Card 
+              title="Kobe Bryant"
+              text="Kobe!!"
+              image={ kobe } />
+            <Card 
+              title="Michael Jordan"
+              text="MJ"
+              image={ mj } />
+          </div>
       </div>
     </div>
-    <SectionCaption>12 sections - 6 hours</SectionCaption>
-    <SectionCellGroup>
-      {staticdata.cells.map(cell => (
-        <Cell title={cell.title} image={cell.image} />
-      ))}
-    </SectionCellGroup>
     </Layout>
 )
 
