3559ae4d707a0a8257974e3343a76ce10facc48a
brianpompey <brianthescholar@gmail.com>
2021-03-16 11:52:55 -0400

    working on resume page now

---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Resume.css'
+import './layout'
+import Layout from './layout';
+
+
+const Resume = () => (
+    <Layout>
+        <div className="ResumeGroup">
+            <p>Resume</p>
+        </div>
+    </Layout>
+)
+
+export default Resume;
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,12 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Resume from "../components/Resume"
 
-const Resume = () => (
+const ResumePage = () => (
     <Layout>
         <SEO title="Resume" />
+        <Resume />
         <Link to="/">Go back to the homepage</Link>
     </Layout>
 )
 
-export default Resume;
+export default ResumePage;
