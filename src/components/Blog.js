ddd7ffb1129a18a0a5d2f2582b3bd54f4ca49651
brianpompey <brianthescholar@gmail.com>
2021-04-19 22:22:06 -0400

    working on the querie for blog posts

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,17 +2,41 @@ import React from 'react'
 import './Blog.css'
 import './layout'
 import Layout from './layout';
+import { useStaticQuery, graphql } from "gatsby";
+import Card from './BlogCard';
 
-const Blog = () => (
-    <Layout>
-        <div className="BlogGroup">
-            
-            <div className="BlogInfo">
-                <h1>COMING SOON!!!</h1>
+const Blog = ({ info }) => {
+    const data = useStaticQuery(graphql`
+        query MyQuery {
+            allContentfulBlogPost {
+                edges {
+                    node {
+                        blogTitle
+                        blogImage {
+                            file {
+                                url
+                            }
+                        }
+                        blogContent {
+                        raw
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    return (
+        <Layout>
+            <div className="BlogGroup">
+                
+                <div className="BlogInfo">
+                    <main><Card data={data} /></main>
+                </div>
+                
             </div>
-            
-        </div>
-    </Layout>
-)
+        </Layout>
+    )
+}
 
 export default Blog;
