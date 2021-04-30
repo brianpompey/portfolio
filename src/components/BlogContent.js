9ca9cef6a223bc640284dde672e7762011171fb4
brianpompey <brianthescholar@gmail.com>
2021-04-30 16:43:27 -0400

    think im gonna ditch the card design for blogs a little complicated right now

---
+++ src/components/BlogContent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './BlogContent.css'
+import moment from 'moment'
+
+
+const BlogContent = ({data}) => (
+    <div className="content">
+        {data.allContentfulBlogPost.edges.map(edge=>(
+            <div className="Card">
+                <h1>{edge.node.blogTitle}</h1>
+                <p>{moment(`${edge.node.dateWritten.toString()}`).format('MMMM Do YYYY')}</p>
+                <p>{edge.node.blogContent.internal.content}</p>
+            </div>          
+        ))} 
+    </div>
+)
+
+export default BlogContent;
