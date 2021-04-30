e398b8b7692dd23e8cbc5b2d305bfa124049793f
brianpompey <brianthescholar@gmail.com>
2021-04-30 16:59:42 -0400

    added more comments

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -5,9 +5,10 @@ import moment from 'moment'
 
 const BlogContent = ({data}) => (
     /* I'd love to have it be sortable and maybe even searchable */
-    <div className="content">
+    /* Maybe it can still show a preview and be expandable though*/
+    <div className="blog-content">
         {data.allContentfulBlogPost.edges.map(edge=>(
-            <div className="Card">
+            <div className="Cons">
                 <h1>{edge.node.blogTitle}</h1>
                 <p>{moment(`${edge.node.dateWritten.toString()}`).format('MMMM Do YYYY')}</p>
                 <p>{edge.node.blogContent.internal.content}</p>
